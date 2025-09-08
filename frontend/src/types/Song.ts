91ce9eabcf9cd174a0f9cb0b3abde300e1f51c3b
ReinaldMendes <reinald.2967@aluno.pr.senac.br>
2025-09-08 17:00:38 -0300

    ok

--- frontend/src/types/Song.ts
+++
@@ -1,8 +0,0 @@
-// src/types/Song.ts
-export interface Song {
-    id: number;
-    title: string;
-    artist: string;
-    youtube_link: string;
-    // Adicione outros campos se necessário, como 'suggested_by', 'is_approved', etc.
-}
