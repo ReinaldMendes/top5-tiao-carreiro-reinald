62f34222273cd9bae7c64bde531237e8f0507755
ReinaldMendes <reinald.2967@aluno.pr.senac.br>
2025-09-08 18:01:06 -0300

    estruturar front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-green-600">
+        Olá, Reinald! React + TypeScript + Tailwind funcionando! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
