293dab7c72873785fbfd08173192f2cfa3ad5859
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2025-09-08 23:34:13 -0300

    ok

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // ajuste conforme seu backend
+  baseURL: "http://localhost:8000/api",
 });
 
 // Interceptor para incluir o token automaticamente
